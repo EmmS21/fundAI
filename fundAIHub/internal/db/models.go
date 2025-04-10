7177f06eec677b8f2691b3e562017b1569a27354
Emmanuel Sibanda <emmanuelsibanda@Emmanuels-MacBook-Air.local>
2025-04-10 18:41:23 -0400

    resolving download issue

--- fundAIHub/internal/db/models.go
+++ fundAIHub/internal/db/models.go
@@ -19,7 +19,7 @@ type Content struct {
 	FilePath    string         `json:"file_path"`
 	Size        int            `json:"size"`
 	StorageKey  sql.NullString `json:"storage_key"`
-	ContentType string         `json:"content_type"`
+	ContentType sql.NullString `json:"content_type"`
 	CreatedAt   time.Time      `json:"created_at"`
 	UpdatedAt   time.Time      `json:"updated_at"`
 }
