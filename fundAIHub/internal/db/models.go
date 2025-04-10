04c5039bef054df2c7523ad55ae458f3643ec61b
Emmanuel Sibanda <emmanuelsibanda@Emmanuels-MacBook-Air.local>
2025-04-10 18:30:36 -0400

    resolving download issue

--- fundAIHub/internal/db/models.go
+++ fundAIHub/internal/db/models.go
@@ -1,26 +1,27 @@
 package db
 
 import (
+	"database/sql"
 	"time"
 
 	"github.com/google/uuid"
 )
 
 type Content struct {
-	ID          uuid.UUID `json:"id"`
-	Name        string    `json:"name"`
-	Type        string    `json:"type"`
-	Version     string    `json:"version"`
-	Description string    `json:"description"`
-	AppVersion  string    `json:"app_version"`
-	ReleaseDate time.Time `json:"release_date"`
-	AppType     string    `json:"app_type"`
-	FilePath    string    `json:"file_path"`
-	Size        int       `json:"size"`
-	StorageKey  string    `json:"storage_key"`
-	ContentType string    `json:"content_type"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	ID          uuid.UUID      `json:"id"`
+	Name        string         `json:"name"`
+	Type        string         `json:"type"`
+	Version     string         `json:"version"`
+	Description string         `json:"description"`
+	AppVersion  string         `json:"app_version"`
+	ReleaseDate time.Time      `json:"release_date"`
+	AppType     string         `json:"app_type"`
+	FilePath    string         `json:"file_path"`
+	Size        int            `json:"size"`
+	StorageKey  sql.NullString `json:"storage_key"`
+	ContentType string         `json:"content_type"`
+	CreatedAt   time.Time      `json:"created_at"`
+	UpdatedAt   time.Time      `json:"updated_at"`
 }
 
 type Download struct {
